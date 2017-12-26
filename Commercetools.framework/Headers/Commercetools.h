c9e8c016a1b4db8b1853a4138b713228945d56f1
Nikola Mladenovic <nikola@mladenovic.biz>
2017-12-26 19:59:03 +0100

    Update Commercetools.framework

---
+++ Commercetools.framework/Headers/Commercetools.h
@@ -0,0 +1,15 @@
+//
+// Copyright (c) 2016 Commercetools. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CommercetoolsSDK.
+FOUNDATION_EXPORT double CommercetoolsVersionNumber;
+
+//! Project version string for CommercetoolsSDK.
+FOUNDATION_EXPORT const unsigned char CommercetoolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Commercetools/PublicHeader.h>
+
+
---
+++ ProductsWithObjC/Examples.m
@@ -0,0 +1,9 @@
+//
+//  Examples.m
+//  ProductsWithObjC
+//
+//  Created by Nikola Mladenovic on 12/26/17.
+//  Copyright © 2017 commercetools GmbH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
